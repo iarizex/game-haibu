fd5cecf98b5e72cafdcafd3a041fe72e342251ea
iarizex <iarizex@protonmail.com>
2024-12-03 12:11:27 +0100

    fix: update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'acento2': '#F8CD2A',
+      'acento1': '#84FF01',
+      'principal': '#234142',
+      'sombras1': '#010D14',
+      'secundario': '#459857',
+      'sombras2': '#585E54',
+      'fondo': '#010409',
+      'texto': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Exo 2', 'sans-serif'],
     extend: {},
   },
   plugins: [],
