122aae18b8070eff695c284149a7410ef3789ae5
iarizex <iarizex@protonmail.com>
2024-12-04 12:57:03 +0100

    feat: create main page and connect with routes

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Main from "./views/Main.vue";
+
+export const routes = [
+    { path: "/", name: "main", component: Main },
+    
+    // { path: "/:path(.*)", component: NotFound },
+  ];
+  
---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Main page</h1>
+    </div>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  name: "Main",
+};
+</script>
