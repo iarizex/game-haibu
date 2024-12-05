8c9ea6df2633bbdf8cf8b5f3862f53ec46de4368
iarizex <iarizex@protonmail.com>
2024-12-05 15:24:27 +0100

    feat: add Game.vue to routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Main from "./views/Main.vue";
+import Game from "./views/Game.vue";
 
 export const routes = [
     { path: "/", name: "main", component: Main },
+    { path: "/game/:id", name: "game", component: Game },
     
     // { path: "/:path(.*)", component: NotFound },
   ];
