c216cc84bccbe6ce291049826bdf61f0577578a9
iarizex <iarizex@protonmail.com>
2024-12-17 19:01:37 +0100

    fix: delete require for vercel and add import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 //const defaultTheme = require("tailwindcss/defaultTheme");
-
+import * as defaultTheme from "tailwindcss/defaultTheme"
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
